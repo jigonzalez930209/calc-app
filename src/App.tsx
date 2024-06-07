c9beb8e726cd46e1e3706020155e8d6aad74a18b
José Iván González Jorge <jigonzalez930209@gmail.com>
2024-06-07 12:03:50 -0300

    feat: Change code style, improve the code length and keep the functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,81 @@
-import { useState } from "react";
-import GlobalStyle1 from "./Styles/theme1";
-import GlobalStyle2 from "./Styles/theme2";
-import GlobalStyle3 from "./Styles/theme3";
+import { useState } from 'react'
+import GlobalStyle1 from './Styles/theme1'
+import GlobalStyle2 from './Styles/theme2'
+import GlobalStyle3 from './Styles/theme3'
 
 import {
+  Button,
+  ButtonContainer,
   Container,
   Header,
-  WrapperSwitch,
-  Switch,
-  SwitcherContainer,
-  Switcher,
   Input,
-  ButtonContainer,
-  Button,
-} from "./App.style";
+  Switch,
+  Switcher,
+  SwitcherContainer,
+  WrapperSwitch,
+} from './App.style'
+
+import buttons from './utils/buttons'
 
 function App() {
-  const [value, setValue] = useState("");
-  const [theme, setTheme] = useState(1);
-  const [themeValue, setThemeValue] = useState("8%");
+  const [value, setValue] = useState('')
+  const [theme, setTheme] = useState(1)
+  const [themeValue, setThemeValue] = useState('8%')
 
-  const hadleTheme = () => {
+  const handleTheme = () => {
     if (theme === 1) {
-      setTheme(2);
-      setThemeValue("38%");
+      setTheme(2)
+      setThemeValue('38%')
     } else if (theme === 2) {
-      setTheme(3);
-      setThemeValue("70%");
+      setTheme(3)
+      setThemeValue('70%')
     } else {
-      setTheme(1);
-      setThemeValue("8%");
+      setTheme(1)
+      setThemeValue('8%')
     }
-  };
-
+  }
 
   const calc = () => {
-    if (value.length >= 5 && value.slice(-1) !== " ") {
-      setValue(eval(value).toString());
+    if (value.length >= 5 && value.slice(-1) !== ' ') {
+      setValue(eval(value).toString())
     }
-  } 
+  }
 
   const deleteValue = () => {
-    if (value.slice(-1) === " ") {
-      setValue(value.substring(0, value.length - 3));
-    } else if (value.slice(-2) === "0.") {
-      setValue(value.substring(0, value.length - 2));
+    if (value.slice(-1) === ' ') {
+      setValue(value.substring(0, value.length - 3))
+    } else if (value.slice(-2) === '0.') {
+      setValue(value.substring(0, value.length - 2))
     } else {
-      setValue(value.substring(0, value.length - 1));
+      setValue(value.substring(0, value.length - 1))
     }
-  };
+  }
 
-  const addSimbol = (simbol: string) => {
-    if (value.slice(-1) !== " " && value.slice(-1) !== ".") {
-      setValue(value + simbol);
-    }
-  };
+  // const addSymbol = (symbol: string) => {
+  //   // Ojo con la ortografía
+  //   if (value.slice(-1) !== ' ' && value.slice(-1) !== '.') {
+  //     setValue(value + symbol)
+  //   }
+  // }
 
+  const handleChangeSetValue = (val = '') => {
+    const operations = ['.', ' - ', ' + ', ' / ', ' * ', 'DEL', '=', 'RES']
+    // OJO Tratar de no utilizar magics strings eso esta mal visto, ejemplo ('RES') esta mal deberia de estar dentro de una
+    // constante global o en un symbol para que no se pueda cambiar al berrete
+    const isOperation = operations.includes(val)
+    console.log({ val, isOperation })
+    if (value.length >= 1 && isOperation) {
+      if (val === 'DEL') deleteValue()
+      else if (val === '=') calc()
+      else if (val === 'RES') setValue('')
+      else
+        setValue((prev) =>
+          prev.slice(-1) !== ' ' && prev.slice(-1) !== '.'
+            ? `${prev}${val}`
+            : prev
+        ) // aqui se hace addSymbol function
+    } else if (!isOperation) setValue((prv) => `${prv}${val}`)
+  }
 
   return (
     <>
@@ -74,7 +94,7 @@ function App() {
                 <span>3</span>
               </div>
 
-              <SwitcherContainer onClick={hadleTheme}>
+              <SwitcherContainer onClick={handleTheme}>
                 <Switcher theme={themeValue} />
               </SwitcherContainer>
             </Switch>
@@ -84,63 +104,75 @@ function App() {
         <Input>{value}</Input>
 
         <ButtonContainer>
-          <Button onClick={() => setValue(value + "7")}>7</Button>
-          <Button onClick={() => setValue(value + "8")}>8</Button>
-          <Button onClick={() => setValue(value + "9")}>9</Button>
-          <Button onClick={() => value.length >= 1 && deleteValue()}
-                  color="var(--white)"
-                  bg="var(--key-background-dark-blue)"
-                  bdbox="var(--key-shadow-dark-blue)"
+          {/* Esto pudiera separarse en un component diferente que haga la pincha
+          de los botones, te puedes fijar que es un patrón que se repite por lo tanto lo que se puede hacer es 
+          crear un array con todos los valores y hacer un array.map((element)=> <Button onClick={() => setValue(value + element)}>{element}</Button>)
+
+          en los botones "especiales"(los que hacen alguna operacion) se puede hacer la decicion en setValue algo como 
+          if()
+          */}
+          {buttons.map((element) => (
+            <Button
+              key={element.label}
+              onClick={() => handleChangeSetValue(element.value)}
+              {...element?.styles}
+            >
+              {element.label}
+            </Button>
+          ))}
+          {/* <Button onClick={() => setValue(value + '7')}>7</Button>
+          <Button onClick={() => setValue(value + '8')}>8</Button>
+          <Button onClick={() => setValue(value + '9')}>9</Button>
+          <Button
+            onClick={() => value.length >= 1 && deleteValue()}
+            color="var(--white)"
+            bg="var(--key-background-dark-blue)"
+            bdBox="var(--key-shadow-dark-blue)"
           >
             DEL
           </Button>
-          <Button onClick={() => setValue(value + "4")}>4</Button>
-          <Button onClick={() => setValue(value + "5")}>5</Button>
-          <Button onClick={() => setValue(value + "6")}>6</Button>
-          <Button onClick={() => value.length >= 1 && addSimbol(" + ")}>
+          <Button onClick={() => setValue(value + '4')}>4</Button>
+          <Button onClick={() => setValue(value + '5')}>5</Button>
+          <Button onClick={() => setValue(value + '6')}>6</Button>
+          <Button onClick={() => value.length >= 1 && addSymbol(' + ')}>
             +
           </Button>
-
-          <Button onClick={() => setValue(value + "1")}>1</Button>
-          <Button onClick={() => setValue(value + "2")}>2</Button>
-          <Button onClick={() => setValue(value + "3")}>3</Button>
-          <Button onClick={() => value.length >= 1 && addSimbol(" - ")}>
+          <Button onClick={() => setValue(value + '1')}>1</Button>
+          <Button onClick={() => setValue(value + '2')}>2</Button>
+          <Button onClick={() => setValue(value + '3')}>3</Button>
+          <Button onClick={() => value.length >= 1 && addSymbol(' - ')}>
             -
           </Button>
-
-          <Button onClick={() => value.length >= 1 && addSimbol(".")}>.</Button>
-          <Button onClick={() => setValue(value + "0")} >0</Button>
-          <Button onClick={() => value.length >= 1 && addSimbol(" / ")}>
+          <Button onClick={() => value.length >= 1 && addSymbol('.')}>.</Button>
+          <Button onClick={() => setValue(value + '0')}>0</Button>
+          <Button onClick={() => value.length >= 1 && addSymbol(' / ')}>
             /
           </Button>
-          <Button onClick={() => value.length >= 1 && addSimbol(" * ")}>
+          <Button onClick={() => value.length >= 1 && addSymbol(' * ')}>
             x
           </Button>
-
           <Button
             gc="1/3"
             color="var(--white)"
             bg="var(--key-background-dark-blue)"
-            bdbox="var(--key-shadow-dark-blue)"
-            onClick={() => setValue("")}
+            bdBox="var(--key-shadow-dark-blue)"
+            onClick={() => setValue('')}
           >
             RESET
           </Button>
-
           <Button
             gc="3/5"
-            color={"var(--white)"}
+            color={'var(--white)'}
             bg="var(--key-background-red)"
-            bdbox="var(--key-shadow-dark-red)"
-
+            bdBox="var(--key-shadow-dark-red)"
             onClick={calc}
           >
             =
-          </Button>
+          </Button> */}
         </ButtonContainer>
       </Container>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
